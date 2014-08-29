6a1f40c104ad76a7cbad67a2d64c5f6feef12718
Sulta <zh.sulta@gmail.com>
2014-08-29 16:39:42 +0600

    add demo templates but not full

---
+++ ui1/app/controllers/Demo.java
@@ -0,0 +1,159 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Demo extends Controller {
+	
+	public static void dashboard() {
+        render("Demo/dashboard.html");
+    }
+	public static void inbox() {
+        render("Demo/inbox.html");
+    }
+	public static void flot() {
+        render("Demo/flot.html");
+    }
+	public static void morris() {
+        render("Demo/morris.html");
+    }
+	public static void inline_charts() {
+        render("Demo/inline_charts.html");
+    }
+	public static void dygraphs() {
+        render("Demo/dygraphs.html");
+    }
+	public static void table() {
+        render("Demo/table.html");
+    }
+	public static void datatables() {
+        render("Demo/datatables.html");
+    }
+	public static void jqgrid() {
+        render("Demo/jqgrid.html");
+    }
+	public static void form_elements() {
+        render("Demo/form_elements.html");
+    }
+	public static void form_templates() {
+        render("Demo/form_templates.html");
+    }
+	public static void validation() {
+        render("Demo/validation.html");
+    }
+	public static void bootstrap_forms() {
+        render("Demo/bootstrap_forms.html");
+    }
+	public static void plugins() {
+        render("Demo/plugins.html");
+    }
+	public static void wizard() {
+        render("Demo/wizard.html");
+    }
+	public static void other_editors() {
+        render("Demo/other_editors.html");
+    }
+	public static void dropzone() {
+        render("Demo/inbox.html");
+    }
+	public static void image_editor() {
+        render("Demo/image_editor.html");
+    }
+	public static void general_elements() {
+        render("Demo/general_elements.html");
+    }
+	public static void buttons() {
+        render("Demo/buttons.html");
+    }
+	public static void fa() {
+        render("Demo/fa.html");
+    }
+	public static void glyph() {
+        render("Demo/glyph.html");
+    }
+	public static void flags() {
+        render("Demo/flags.html");
+    }
+	public static void grid() {
+        render("Demo/grid.html");
+    }
+	public static void treeview() {
+        render("Demo/treeview.html");
+    }
+	public static void nestable_list() {
+        render("Demo/nestable_list.html");
+    }
+	public static void jqui() {
+        render("Demo/jqui.html");
+    }
+	public static void typography() {
+        render("Demo/typography.html");
+    }
+	public static void calendar() {
+        render("Demo/calendar.html");
+    }
+	public static void widgets() {
+        render("Demo/widgets.html");
+    }
+	public static void gallery() {
+        render("Demo/gallery.html");
+    }
+	public static void gmap_xml() {
+        render("Demo/gmap_xml.html");
+    }
+	public static void forum() {
+        render("Demo/forum.html");
+    }
+	public static void profile() {
+        render("Demo/profile.html");
+    }
+	public static void timeline() {
+        render("Demo/timeline.html");
+    }
+	public static void pricing_table() {
+        render("Demo/pricing_table.html");
+    }
+	public static void invoice() {
+        render("Demo/invoice.html");
+    }
+	public static void login() {
+        render("Demo/login.html");
+    }
+	public static void register() {
+        render("Demo/register.html");
+    }
+	public static void lock() {
+        render("Demo/lock.html");
+    }
+	public static void error500() {
+        render("Demo/error500.html");
+    }
+	public static void error404() {
+        render("Demo/error404.html");
+    }
+	public static void blank() {
+        render("Demo/blank.html");
+    }
+	public static void email_template() {
+        render("Demo/email_template.html");
+    }
+	public static void search() {
+        render("Demo/search.html");
+    }
+	public static void ckeditor() {
+        render("Demo/ckeditor.html");
+    }
+	public static void difver() {
+        render("Demo/difver.html");
+    }
+	public static void applayout() {
+        render("Demo/applayout.html");
+    }
+	public static void forgotpassword() {
+        render("Demo/forgotpassword.html");
+    }
+}
