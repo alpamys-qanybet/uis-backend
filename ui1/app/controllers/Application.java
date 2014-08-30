0dedd0e2493bd7fb6cc7ec637d5266b30bcf9509
Sulta <zh.sulta@gmail.com>
2014-08-30 14:28:56 +0600

    login with secure model added

--- ui1/app/controllers/Application.java
+++ ui1/app/controllers/Application.java
@@ -1,14 +1,26 @@
 package controllers;
 
 import play.*;
+import play.libs.Crypto;
 import play.mvc.*;
 
 import java.util.*;
 
+import controllers.Secure.Security;
+
 import models.*;
 
+@With(Secure.class)
 public class Application extends Controller {
-
+	
+	@Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+//            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", "email");
+        }
+    }
+	
     public static void index() {
         render();
     }
