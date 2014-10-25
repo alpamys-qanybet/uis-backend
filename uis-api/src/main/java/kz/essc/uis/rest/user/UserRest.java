bdf02b6adcc9ce7be3effb003b82c5f3e2d0f8d4
Alpamys <alpamys.kanibetov@gmail.com>
2014-10-25 18:11:37 +0600

    security src code

--- uis-api/src/main/java/kz/essc/uis/rest/user/UserRest.java
+++ uis-api/src/main/java/kz/essc/uis/rest/user/UserRest.java
@@ -1,8 +1,12 @@
 package kz.essc.uis.rest.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,6 +15,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import kz.essc.uis.bean.user.UserBean;
@@ -23,6 +28,12 @@ public class UserRest {
 	
 	@Inject
 	UserBean userBean;
+	
+	@Context
+	HttpServletRequest request;
+	
+	@Context
+	HttpServletResponse response;
 	
 	@GET
 	@Path("/")
@@ -57,7 +68,7 @@ public class UserRest {
 	
 	@PUT
 	@Path("/{id}")
-	public UserWrapper edit(@PathParam("id") Long id, UserWrapper userWrapper) {
+	public UserWrapper edit(@PathParam("id") Long id, UserWrapper userWrapper) throws IOException {
 		return userBean.edit(id, userWrapper);
 	}
 	
@@ -76,6 +87,54 @@ public class UserRest {
 		catch (Exception e) {
 			e.printStackTrace();
 			return null;
+		}
+	}
+	
+	/*@POST
+	@Path("/login")
+	public boolean login(UserWrapper user) {
+		System.out.println("login");
+	
+		try {
+			HttpSession session = request.getSession();
+			
+			request.authenticate(response);
+
+			System.out.println(request.getUserPrincipal().getName());
+			System.out.println( session.getId() );
+			
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			
+			return false;
+		}
+	}*/
+	
+	@GET
+	@Path("/logout")
+	public boolean logout() {
+		System.out.println("logout");
+	
+		try {
+			HttpSession session = request.getSession();
+			System.out.println("before");
+			System.out.println(request.getUserPrincipal().getName());
+			System.out.println( session.getId() );
+			
+			request.logout();
+
+			System.out.println("after");
+			System.out.println(request.getUserPrincipal().getName());
+			System.out.println( session.getId() );
+			
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			
+			return false;
 		}
 	}
 }
